eea1569598a4b7ddd587181da71cae012a50a8d0
dorgon.chang <dorgonman@hotmail.com>
2018-05-27 14:06:12 +0800

    up

---
+++ git_reset_master.sh
@@ -0,0 +1,4 @@
+git fetch origin
+git submodule foreach --recursive "git fetch origin"
+git checkout -f -B master origin/master
+git submodule foreach --recursive "git checkout -f -B master origin/master"
