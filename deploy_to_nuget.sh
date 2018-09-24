2c88cfeb5bb9e2acf7049c5f48f4265284ae7859
dorgon.chang <dorgonman@hotmail.com>
2018-09-25 00:02:35 +0800

    fix nuget deploy

--- deploy_to_nuget.sh
+++ deploy_to_nuget.sh
@@ -12,18 +12,18 @@ echo ${cmd}
 eval ${cmd}
 
 
-
+rm -rf *.nupkg
 cmd=" \
 nuget pack nuspec/win64/${PACKAGE_NAME}.nuspec \
 "
 
 echo ${cmd}
-#eval ${cmd} 
-
-
+eval ${cmd} 
+NUPKG_FILE_NAME=$(find *.nupkg)
+NUPKG_NAME=${NUPKG_FILE_NAME%.*}
 cmd=" \
-nuget push -Source ${PACKAGE_NAME} -ApiKey VSTS ${PACKAGE_NAME}.0.0.0.nupkg \
+nuget push -Source ${PACKAGE_NAME} -ApiKey VSTS ${NUPKG_NAME}.nupkg \
 "
 
 echo ${cmd}
-#eval ${cmd}
+eval ${cmd}
