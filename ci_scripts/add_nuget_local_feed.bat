b69cf858574d56ce596fd1d4540c52e18e3acd00
dorgon.chang <dorgonman@hotmail.com>
2018-09-26 23:40:09 +0800

    up

--- ci_scripts/add_nuget_local_feed.bat
+++ ci_scripts/add_nuget_local_feed.bat
@@ -1,6 +1,6 @@
 set SCRIPT_DIR=%~dp0
 pushd %SCRIPT_DIR%
 cd ..
-ue_ci_scripts\bin\nuget\nuget sources remove -Name %FEED_NAME%
-ue_ci_scripts\bin\nuget\nuget sources Add -Name %FEED_NAME% -Source \\hsgame\UE4-Packaged-build\nuget\%FEED_NAME%
+ue_ci_scripts\bin\nuget\nuget sources remove -Name hsgame-local
+ue_ci_scripts\bin\nuget\nuget sources Add -Name hsgame-local -Source \\hsgame\UE4-Packaged-build\nuget\hsgame-local
 popd
