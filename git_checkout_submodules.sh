95959667e14fb5ee3ea7c5a7ccbf1dc962e1cec7
dorgon chang <dorgon.chang@horizon-studio.net>
2019-09-22 13:57:04 +0000

    Merged PR 50: [Feature] HorizonRadioButton
    Related work items: #974

--- git_checkout_submodules.sh
+++ git_checkout_submodules.sh
@@ -4,7 +4,7 @@ set -e
 GIT_BRANCH=$(git symbolic-ref --short HEAD)
 
 git submodule foreach --recursive "git checkout -f master"
-git submodule foreach --recursive "git pull"
+git submodule foreach --recursive "git pull --tags --prune"
 
 git submodule foreach --recursive "git checkout -f ${GIT_BRANCH} || :"
 git submodule foreach --recursive "git pull"
