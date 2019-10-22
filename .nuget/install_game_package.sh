db3789fadeace75c63a7d7bde18a3fd6a761bc38
dorgon chang <dorgonman@hotmail.com>
2019-10-22 21:35:08 +0800

    cache nuget package and copy target package from cache

--- .nuget/install_game_package.sh
+++ .nuget/install_game_package.sh
@@ -6,21 +6,24 @@ pushd ..
 popd
 export PACKAGE_NAME=${PROJECT_NAME}
 export FEED_NAME=$1
-export OUTPUT_DIRECTORY=$(cd ../../; pwd)
+export OUTPUT_DIRECTORY=$(cd ../; pwd)
+export CACHE_DIRECTORY=../Intermediate/NuGetCache/
+mkdir -p ${CACHE_DIRECTORY}
+CACHE_DIRECTORY=$(cd ../Intermediate/NuGetCache/;pwd)
 
 echo OUTPUT_DIRECTORY: ${OUTPUT_DIRECTORY}
+echo CACHE_DIRECTORY: ${CACHE_DIRECTORY}
 echo PACKAGE_NAME: ${PACKAGE_NAME}
 echo FEED_NAME: ${FEED_NAME}
-pushd ${OUTPUT_DIRECTORY}/${PACKAGE_NAME}
+pushd .. > /dev/null
     git fetch --prune --tags origin
     git tag -l --points-at HEAD
     export PACKAGE_VERISON=$(git tag -l --points-at HEAD | grep -a "editor/hsgame/" | grep -Eo '[.0-9]*{1,9}')
-popd
+popd > /dev/null
 
 
 if [ -n "${PACKAGE_VERISON}" ]; then
     INSTALL_VERSION="-Version ${PACKAGE_VERISON}"
-
 else
     INSTALL_VERSION=' '
     # echo "[Error] Can't find nuget version tag here:"
@@ -34,10 +37,24 @@ fi
 
 cmd=" \
     nuget install ${PACKAGE_NAME} ${INSTALL_VERSION} \
-    -OutputDirectory ${OUTPUT_DIRECTORY} -PackageSaveMode nuspec \
-    -Source ${FEED_NAME} \
-    -ExcludeVersion -ForceEnglishOutput  \
+    -OutputDirectory ${CACHE_DIRECTORY} -PackageSaveMode nuspec -Force \
+    -Source ${FEED_NAME} -Verbosity detailed \
+     -ForceEnglishOutput  \
     "
-#-Verbosity detailed
+#-ExcludeVersion
 echo ${cmd}
 eval ${cmd} 
+
+if [ "${INSTALL_VERSION}" != ' ' ]; then
+    nugetFolder=../Intermediate/NugetCache/${PACKAGE_NAME}.${PACKAGE_VERISON}
+    
+    echo "Using current nuget pacakge from cache:"
+else
+    nugetFolder=$(ls -td -- ../Intermediate/NugetCache/* | tail -n 1)
+    echo "Using latest nuget pacakge from cache:"
+fi
+nugetFolder=$(cd ${nugetFolder}; pwd)
+echo "${nugetFolder}"
+cmd="cp -rf ${nugetFolder}/* ${OUTPUT_DIRECTORY}"
+echo ${cmd}
+eval ${cmd}
--- .nuget/install_game_package_from_hsgame-locall.sh
+++ .nuget/install_game_package_from_hsgame-locall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-FEED_NAME="//HSGAME/UE4-Packaged-build/nuget/hsgame-local/GameProject/"
+FEED_NAME="//HSGAME/UE4-Packaged-build/nuget/hsgame-local/HorizonPlugin/"
 ./install_game_package.sh ${FEED_NAME}
