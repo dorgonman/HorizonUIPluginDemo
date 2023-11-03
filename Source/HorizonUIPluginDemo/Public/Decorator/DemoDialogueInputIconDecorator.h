416fd20b7ebe8e66f98e1fb68fc134d7865ff467
dorgon.chang <dorgonman@hotmail.com>
2023-11-04 00:08:23 +0800

    AB#2986 [BugFix] Fix Warning: Property ObjectProperty contains short type name

--- Source/HorizonUIPluginDemo/Public/Decorator/DemoDialogueInputIconDecorator.h
+++ Source/HorizonUIPluginDemo/Public/Decorator/DemoDialogueInputIconDecorator.h
@@ -15,9 +15,14 @@ struct HORIZONUIPLUGINDEMO_API FDemoInputImageTableRow : public FTableRowBase
 	GENERATED_BODY()
 public:
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Content", meta = (AllowPrivateAccess = "true", DisplayThumbnail = "true", AllowedClasses = "Texture,MaterialInterface,PaperSprite,PaperFlipbook", DisallowedClasses = "MediaTexture"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Content", meta = (AllowPrivateAccess = "true", 
+		DisplayThumbnail = "true", AllowedClasses = "/Script/Engine.Texture, /Script/Engine.MaterialInterface, /Script/Paper2D.PaperSprite, /Script/Paper2D.PaperFlipbook", 
+		DisallowedClasses = "/Script/MediaAssets.MediaTexture"))
 	FSoftObjectPath KeyboardAndMouse;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Content", meta = (AllowPrivateAccess = "true", DisplayThumbnail = "true", AllowedClasses = "Texture,MaterialInterface,PaperSprite,PaperFlipbook", DisallowedClasses = "MediaTexture"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Content", meta = (AllowPrivateAccess = "true", 
+		DisplayThumbnail = "true", 
+		AllowedClasses = "/Script/Engine.Texture, /Script/Engine.MaterialInterface, /Script/Paper2D.PaperSprite, /Script/Paper2D.PaperFlipbook", 
+		DisallowedClasses = "/Script/MediaAssets.MediaTexture"))
 	FSoftObjectPath XBoxController;
 
 
